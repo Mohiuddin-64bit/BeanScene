5e245de4e3f0050eb4f0f785c3d7b9ca473b449c
Mohiuddin <mohiuddin.niddu@gmail.com>
2023-12-17 02:07:59 +0600

    Update title and navbar styling

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,79 +1,85 @@
 const Navbar = () => {
   return (
-    
-    <div className="navbar bg-transparent absolute">
-      <div className="navbar-start">
-        <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />
-            </svg>
+    <header className="container ">
+      <div className="drawer md:px-12 p-3 px-0 absolute">
+        <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content flex flex-col">
+          {/* Navbar */}
+          <div className="w-full navbar grid grid-cols-3 ">
+            <div className="flex-none lg:hidden">
+              <label
+                htmlFor="my-drawer-3"
+                aria-label="open sidebar"
+                className="btn btn-square custom-btn"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  className="inline-block w-6 h-6 stroke-current"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  ></path>
+                </svg>
+              </label>
+            </div>
+            <div className="px-2 text-4xl col-span-2 md:col-span-1 mx-2 font-clicker  text-white">
+              Bean Scene
+            </div>
+            <div className="hidden lg:block mx-auto">
+              <ul className="menu menu-horizontal">
+                <li className="text-white">
+                  <a>Home</a>
+                </li>
+                <li className="text-white">
+                  <a>Menu</a>
+                </li>
+                <li className="text-white">
+                  <a>About Us</a>
+                </li>
+                <li className="text-white">
+                  <a>Contact Us</a>
+                </li>
+              </ul>
+            </div>
+            <div className="ml-auto hidden md:block">
+              <a className="custom-btn">Button</a>
+            </div>
           </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 rounded-box w-52"
-          >
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <a>Parent</a>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
-          </ul>
+          {/* Page content here */}
         </div>
-        <a className="btn btn-ghost text-white text-4xl font-clicker">
-          Bean Scene
-        </a>
-      </div>
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-          <li>
-            <a>Item 1</a>
-          </li>
-          <li>
-            <details>
-              <summary>Parent</summary>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
+        <div className="drawer-side z-50">
+          <label
+            htmlFor="my-drawer-3"
+            aria-label="close sidebar"
+            className="drawer-overlay"
+          ></label>
+          <div className="bg-[#FFF9F1] ">
+            <div className="flex justify-center min-h-screen">
+              <ul className="menu p-4 w-80 text-2xl items-center justify-center min-h-full ">
+                {/* Sidebar content here */}
+                <li >
+                  <a>Home</a>
                 </li>
-                <li>
-                  <a>Submenu 2</a>
+                <li >
+                  <a>Menu</a>
+                </li>
+                <li >
+                  <a>About Us</a>
+                </li>
+                <li >
+                  <a>Contact Us</a>
                 </li>
               </ul>
-            </details>
-          </li>
-          <li>
-            <a>Item 3</a>
-          </li>
-        </ul>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="navbar-end">
-        <a className="btn">Button</a>
-      </div>
-    </div>
+    </header>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-color': '#F9C06A',
+        'customColor2': '#abcdef',
+      },
+    },
     fontFamily: {
       "clicker": ["Clicker Script", "cursive"],
+      "playfair": ["Playfair Display", "serif"],
     },
     daisyui: {
       themes: ["light", "dark","coffee",],
